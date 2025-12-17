4b5466e4ccb132095b79c0a3cfab80c09b2ed1da
victorayalauveg <24012066@es.uveg.edu.mx>
2025-12-16 19:54:47 -0700

    Add files via upload

---
+++ PruebaGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagit;
+
+/**
+ *
+ * @author victo
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("¡Hola mundo!");
+    }
+    
+}
