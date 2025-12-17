cb6bf43099572779bd2094f0d1adaa3f4600d231
Victor Ayala <24012066@es.uveg.edu.mx>
2025-12-16 20:14:31 -0700

    Cambio de mensaje

--- PruebaGit.java
+++ PruebaGit.java
@@ -16,6 +16,7 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("¡Hola nuevo repositorio!");
+        // Agregamos un nuevo mensaje
     }
     
 }
